eda5020dbf74777c370a24e6235bf88197990d12
Mohammed Minhaj <nijulinux@gmail.com>
2025-08-24 16:07:34 +0530

    feat:(gin) http server setup

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -10,5 +12,16 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading env: ", err)
 	}
+
+	PORT := os.Getenv("PORT")
+
+	if PORT == "" {
+		PORT = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.Run(":" + PORT)
 
 }
