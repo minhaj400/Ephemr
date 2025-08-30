b62dceec2fe002cbabd09c8c2ee48a23ed46af6f
Mohammed Minhaj <nijulinux@gmail.com>
2025-08-30 12:00:09 +0530

    feat(pkg): password utils refactored

---
+++ pkg/crypto/password.go
@@ -0,0 +1,29 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+type PasswordHasher interface {
+	Hash(password string) (string, error)
+	Compare(password, hash string) bool
+}
+
+type BcryptHasher struct {
+	cost int
+}
+
+func NewBcryptHasher(cost int) PasswordHasher {
+	return &BcryptHasher{cost: cost}
+}
+
+func (b *BcryptHasher) Hash(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), b.cost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func (b *BcryptHasher) Compare(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
