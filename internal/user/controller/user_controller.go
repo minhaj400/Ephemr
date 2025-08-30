d812c2f4bdc8a5e84caf8138ee1aa046ca65de81
Mohammed Minhaj <nijulinux@gmail.com>
2025-08-30 16:28:05 +0530

    feat(user): user module created

---
+++ internal/user/controller/user_controller.go
@@ -0,0 +1,13 @@
+package controller
+
+import "github.com/Minhajxdd/Ephemr/internal/user/service"
+
+type UserController interface{}
+
+type userController struct {
+	srv *service.UserService
+}
+
+func NewUserService(srv *service.UserService) UserController {
+	return &userController{srv: srv}
+}
---
+++ internal/user/module.go
@@ -0,0 +1,22 @@
+package user
+
+import (
+	"github.com/Minhajxdd/Ephemr/internal/user/controller"
+	repositories "github.com/Minhajxdd/Ephemr/internal/user/repository"
+	"github.com/Minhajxdd/Ephemr/internal/user/service"
+	"gorm.io/gorm"
+)
+
+type Module struct {
+	Controller *controller.UserController
+}
+
+func NewUserModule(db *gorm.DB) *Module {
+	repo := repositories.NewUserRepository(db)
+	srv := service.NewUserService(&repo)
+	ctrl := controller.NewUserService(&srv)
+
+	return &Module{
+		Controller: &ctrl,
+	}
+}
