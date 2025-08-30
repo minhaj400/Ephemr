0f2db7802c663f9e13b406e6bf03403e0b5158dd
Mohammed Minhaj <nijulinux@gmail.com>
2025-08-30 14:42:00 +0530

    feat(user): user repo and model

---
+++ internal/user/repository/user_repository.go
@@ -0,0 +1,40 @@
+package repositories
+
+import (
+	"github.com/Minhajxdd/Ephemr/internal/user/model"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *model.User) error
+	FindByEmail(email string) (*model.User, error)
+	GetByID(id uint) (*model.User, error)
+}
+
+type userRepo struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepo{db: db}
+}
+
+func (r *userRepo) Create(u *model.User) error {
+	return r.db.Create(u).Error
+}
+
+func (r *userRepo) FindByEmail(email string) (*model.User, error) {
+	var u model.User
+	if err := r.db.Where(&model.User{Email: email}).First(&u).Error; err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
+
+func (r *userRepo) GetByID(id uint) (*model.User, error) {
+	var u model.User
+	if err := r.db.First(&u, id).Error; err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
