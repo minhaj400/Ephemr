0972a2e7e75dac3f01ff7bf869d65cfe129e69fa
Mohammed Minhaj <nijulinux@gmail.com>
2025-09-13 12:51:37 +0530

    feat(auth): find with token device ip

--- internal/auth/repository/refresh_token_repository.go
+++ internal/auth/repository/refresh_token_repository.go
@@ -1,12 +1,15 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/Minhajxdd/Ephemr/internal/auth/model"
 	"gorm.io/gorm"
 )
 
 type RefreshTokenRepository interface {
 	Create(rt *model.RefreshTokens) error
+	FindWithTokenDeviceIp(token, device, ipAddress string) (*model.RefreshTokens, error)
 }
 
 type refreshTokenRepo struct {
@@ -19,4 +22,19 @@ func NewRefreshTokenRepo(db *gorm.DB) RefreshTokenRepository {
 
 func (r *refreshTokenRepo) Create(rt *model.RefreshTokens) error {
 	return r.db.Create(rt).Error
+}
+
+func (r *refreshTokenRepo) FindWithTokenDeviceIp(token, device, ipAddress string) (*model.RefreshTokens, error) {
+	var rt model.RefreshTokens
+	if err := r.db.Where(&model.RefreshTokens{
+		TokenHash: token,
+		Device:    device,
+		IpAddress: ipAddress,
+	}).First(&rt).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &rt, nil
 }
